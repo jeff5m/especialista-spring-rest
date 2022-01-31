32030d08f8f0c943a13228fcaca651095abc7827
jeff5m <jeff.matheus18@gmail.com>
2022-01-30 23:08:17 -0300

    feat: resolve beans ambiguity using @Qualifier

--- src/main/java/com/algaworks/algafood/di/service/ClientActivationService.java
+++ src/main/java/com/algaworks/algafood/di/service/ClientActivationService.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.di.service;
 import com.algaworks.algafood.di.model.Client;
 import com.algaworks.algafood.di.notification.Notifier;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,7 @@ public class ClientActivationService {
     private final Notifier notifier;
 
     @Autowired
-    public ClientActivationService(Notifier notifier) {
+    public ClientActivationService(@Qualifier("urgent") Notifier notifier) {
         this.notifier = notifier;
     }
 
