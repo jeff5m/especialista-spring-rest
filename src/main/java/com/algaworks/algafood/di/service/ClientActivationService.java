34ab068b8113376b4ee660bcb3c21f93747fe007
jeff5m <jeff.matheus18@gmail.com>
2022-02-01 23:05:17 -0300

    feat: define callback methods for beans lifecyle implementing interfaces inside a bean class

--- src/main/java/com/algaworks/algafood/di/service/ClientActivationService.java
+++ src/main/java/com/algaworks/algafood/di/service/ClientActivationService.java
@@ -6,9 +6,13 @@ import com.algaworks.algafood.di.notification.NotifierType;
 import com.algaworks.algafood.di.notification.PriorityLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class ClientActivationService {
+@Service
+public class ClientActivationService implements InitializingBean, DisposableBean {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ClientActivationService.class);
 
@@ -18,18 +22,18 @@ public class ClientActivationService {
     public ClientActivationService(@NotifierType(PriorityLevel.NORMAL) Notifier notifier) {
         this.notifier = notifier;
     }
-
-    public void init() {
-        LOGGER.warn("First method of bean lifecycle");
-    }
-
-    public void destroy() {
-        LOGGER.warn("Last method of bean lifecycle");
-    }
-
     public void activate(Client client) {
         client.activate();
         this.notifier.notify(client, "Your registration is active!");
     }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        LOGGER.warn("First method of bean lifecycle");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        LOGGER.warn("Last method of bean lifecycle");
+    }
 }
