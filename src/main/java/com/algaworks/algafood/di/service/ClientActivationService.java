8b80017b76fece28339e4e32eefa50a510307cdd
jeff5m <jeff.matheus18@gmail.com>
2022-02-01 22:59:54 -0300

    feat: define callback methods for beans lifecyle inside the bean class

--- src/main/java/com/algaworks/algafood/di/service/ClientActivationService.java
+++ src/main/java/com/algaworks/algafood/di/service/ClientActivationService.java
@@ -4,17 +4,34 @@ import com.algaworks.algafood.di.model.Client;
 import com.algaworks.algafood.di.notification.Notifier;
 import com.algaworks.algafood.di.notification.NotifierType;
 import com.algaworks.algafood.di.notification.PriorityLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Service
 public class ClientActivationService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientActivationService.class);
 
     private final Notifier notifier;
 
     @Autowired
     public ClientActivationService(@NotifierType(PriorityLevel.NORMAL) Notifier notifier) {
         this.notifier = notifier;
+    }
+
+    @PostConstruct // called after bean instantiation
+    public void init() {
+        LOGGER.warn("First method of bean lifecycle");
+    }
+
+    @PreDestroy // called right before bean deletion
+    public void destroy() {
+        LOGGER.warn("Last method of bean lifecycle");
     }
 
     public void activate(Client client) {
